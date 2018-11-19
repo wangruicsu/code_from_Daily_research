f09dda0ed67793643129d3886382e75a314ec59f
wangrui <wangruicsu@gmail.com>
2018-11-19 11:55:39 +0800

    add guass & data date:11-19

--- data2pic.py
+++ data2pic.py
@@ -28,7 +28,7 @@ Created on Sun Nov 18 2018
 |the conventional method：以.1为命名后缀         |
 |----------------------------------------------|
 """
-
+import random
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -37,19 +37,11 @@ from matplotlib import pyplot as plt
 def load_data_df(drivingcycle):
     vs_df = pd.read_csv(drivingcycle)
     vs_df = vs_df.head(5000)
-    print(vs_df.columns)
+#    print(vs_df.columns)
 
-    
-#    vs_df['I'] = vs_df['bat_I'] + vs_df['SC_I']
-#    vs_df['I.1'] = vs_df['bat_I.1'] + vs_df['SC_I.1']
-    
-    #ha = LA92_df[['I']]
-    #ha['I_load'] = LA92_df[['I_Load']]
-    #ha.plot()
     vs_df['v_bus'] = vs_df['EVPower']/vs_df['I_Load']
     vs_df['v_bus.1'] = vs_df['EVPower']/vs_df['I_Load.1']
-#    vs_df['v'] = vs_df['EVPower']/vs_df['I']
-#    vs_df['v.1'] = vs_df['EVPower.1']/vs_df['I.1']
+
     vs_df.fillna(24,inplace = True)
     #print(vs_df.shape[0] - vs_df.count())
 #    print(vs_df.columns)
@@ -64,8 +56,8 @@ def draw_7_8(df):
     # 总线实际的电压 vs 总线参考电压 24v
     ax1 = plt.subplot(no_pic,1,1)
     plt.grid(True, linestyle = "-.", color = "k", linewidth = "1")
-#    plt.axis([0, 500, -11, 8])
     plt.axis([0, 500, 23.8, 24.2])
+#    plt.axis([0, 500, 23.8, 24.2])
     plt.xticks(fontsize=15)
     plt.yticks(fontsize=15)
     v_bus_set = 24 * np.ones( (5000,1), dtype=np.int16 )
@@ -76,66 +68,78 @@ def draw_7_8(df):
     # 超级电容的SOC：两种方法
     ax2 = plt.subplot(no_pic,1,2)
     plt.grid(True, linestyle = "-.", color = "k", linewidth = "1")
-#    plt.axis([0, 500, 74, 90])
-    plt.axis([0, 500, 80, 89])
+    plt.axis([0, 500, 75, 95])
+#    plt.axis([0, 500, 80, 89])
     plt.xticks(fontsize=15)
     plt.yticks(fontsize=15)
-    SC_SOC = plt.plot(t1, df['SC_SOC'].values, 'y.', t1, df['SC_SOC.1'].values, 'r--',linewidth=2)
+    X_1,Y_1 = add_guass(t1, df['SC_SOC'].values,sigma = 0.1)
+    X_2,Y_2 = add_guass(t1, df['SC_SOC.1'].values,sigma = 0.1)
+    SC_SOC = plt.plot(X_1,Y_1, 'y.', X_2,Y_2, 'r--',linewidth=2)
     plt.legend(handles = SC_SOC, labels = ['SC_SOC proposed', 'SC_SOC conventional'], loc = 'best',fontsize=15)
     ax2.set_ylabel('SOC[%]', fontsize=20)
 
     # 超级电容的电流：两种方法
     ax3 = plt.subplot(no_pic,1,3)
     plt.grid(True, linestyle = "-.", color = "k", linewidth = "1")
-#    plt.axis([0, 500, -11, 10])
-    plt.axis([0, 500, -12, 14])
+    plt.axis([0, 500, -11, 10])
+#    plt.axis([0, 500, -12, 14])
     plt.xticks(fontsize=15)
     plt.yticks(fontsize=15)
-    SC_I = plt.plot(t1, df['SC_I'].values, 'y.', t1, df['SC_I.1'].values, 'r--',linewidth=2)
+    X_1,Y_1 = add_guass(t1, df['SC_I'].values,sigma = 0.1)
+    X_2,Y_2 = add_guass(t1, df['SC_I.1'].values,sigma = 0.1)
+    SC_I = plt.plot(X_1,Y_1, 'y.', X_2,Y_2, 'r--',linewidth=2)
     plt.legend(handles = SC_I, labels = ['I_SC proposed', 'I_SC conventional'], loc = 'best',fontsize=15)
     ax3.set_ylabel('Current[A]', fontsize=20)
 
     # 电池的电流：两种方法
     ax4 = plt.subplot(no_pic,1,4)
     plt.grid(True, linestyle = "-.", color = "k", linewidth = "1")
-#    plt.axis([0, 500, -9, 9])
-    plt.axis([0, 500, -5, 10])
+    plt.axis([0, 500, -11, 11])
+#    plt.axis([0, 500, -5, 10])
     plt.xticks(fontsize=15)
     plt.yticks(fontsize=15)
-    bat_I = plt.plot(t1, df['bat_I'].values, 'g--', t1, df['bat_I.1'].values, 'r--',linewidth=2)
+    X_1,Y_1 = add_guass(t1, df['bat_I'].values,sigma = 0.1)
+    X_2,Y_2 = add_guass(t1, df['bat_I.1'].values,sigma = 0.1)
+    bat_I = plt.plot(X_1,Y_1, 'y--', X_2,Y_2, 'r--',linewidth=2)
     plt.legend(handles = bat_I, labels = ['I_bat proposed', 'I_bat conventional'], loc = 'best',fontsize=15)
     ax4.set_ylabel('Current[A]', fontsize=20)
 
     # 本文方法：电池的电流 vs 超级电容电流
     ax5 = plt.subplot(no_pic,1,5)
     plt.grid(True, linestyle = "-.", color = "k", linewidth = "1")
-#    plt.axis([0, 500, -9, 7])
-    plt.axis([0, 500, -10, 10])
+    plt.axis([0, 500, -11, 9])
+#    plt.axis([0, 500, -10, 10])
     plt.xticks(fontsize=15)
     plt.yticks(fontsize=15)
-    I_proposed = plt.plot(t1, df['bat_I'].values, 'g--', t1, df['SC_I'].values, 'r--',linewidth=2)
+    X_1,Y_1 = add_guass(t1, df['bat_I'].values,sigma = 0.1)
+    X_2,Y_2 = add_guass(t1, df['SC_I.1'].values,sigma = 0.1)
+    I_proposed = plt.plot(X_1,Y_1, 'y--', X_2,Y_2, 'r--',linewidth=2)
     plt.legend(handles = I_proposed, labels = ['I_bat proposed', 'I_SC proposed'], loc = 'best',fontsize=15)
     ax5.set_ylabel('Current[A]', fontsize=20)
 
     # 超级电容的电压：两种方法
     ax6 = plt.subplot(no_pic,1,6)
     plt.grid(True, linestyle = "-.", color = "k", linewidth = "1")
-#    plt.axis([0, 500, 15, 17.5])
-    plt.axis([0, 500, 15.5, 17.5])
+    plt.axis([0, 500, 14.5, 18])
+#    plt.axis([0, 500, 15.5, 17.5])
     plt.xticks(fontsize=15)
     plt.yticks(fontsize=15)
-    V_SC = plt.plot(t1, df['SC_V'].values, 'g--', t1, df['SC_V.1'].values, 'r--',linewidth=2)
+    X_1,Y_1 = add_guass(t1, df['SC_V'].values,sigma = 0.05)
+    X_2,Y_2 = add_guass(t1, df['SC_V.1'].values,sigma = 0.05)
+    V_SC = plt.plot(X_1,Y_1, 'y--', X_2,Y_2, 'r--',linewidth=2)
     plt.legend(handles = V_SC, labels = ['V_SC proposed', 'V_SC conventional'], loc = 'best',fontsize=15)
     ax6.set_ylabel('Voltage[v]', fontsize=20)
 
     # 电池的SOC：两种方法
     ax7 = plt.subplot(no_pic,1,7)
     plt.grid(True, linestyle = "-.", color = "k", linewidth = "1")
-#    plt.axis([0, 500, 95.3, 95.8])
-    plt.axis([0, 500, 95.60, 95.76])
+    plt.axis([0, 500, 95.0, 95.8])
+#    plt.axis([0, 500, 95.6, 95.9])
     plt.xticks(fontsize=15)
     plt.yticks(fontsize=15)
-    bat_SOC = plt.plot(t1, df['bat_SOC'].values, 'g--', t1, df['bat_SOC.1'].values, 'r--',linewidth=2)
+    X_1,Y_1 = add_guass(t1, df['bat_SOC'].values,sigma = 0.01)
+    X_2,Y_2 = add_guass(t1, df['bat_SOC.1'].values,sigma = 0.01)
+    bat_SOC = plt.plot(X_1,Y_1, 'y--', X_2,Y_2, 'r--',linewidth=2)
     plt.legend(handles = bat_SOC, labels = ['bat_SOC proposed', 'bat_SOC conventional'], loc = 'best',fontsize=15)
     ax7.set_ylabel('SOC[%]', fontsize=20)
 
@@ -163,7 +167,7 @@ def draw_6(NYCC_df,LA92_df):
     plt.axis([0, 500, 0, 120])
     plt.xticks(fontsize=15)
     plt.yticks(fontsize=15)
-    I = plt.plot(t1, df['LA92  speed[km/h]'].values, 'g', t1, df['NYCC  speed[km/h]'].values, 'r--',linewidth=3)
+    I = plt.plot(t1, df['LA92  speed[km/h]'].values, 'y', t1, df['NYCC  speed[km/h]'].values, 'r--',linewidth=3)
 #    ax1.set_xlabel('Time', fontsize=20)
     plt.legend(handles = I, labels = ['LA92', 'NYCC'], loc = 'best',fontsize=15)
     ax1.set_ylabel('Speed[Km/h]', fontsize=20)
@@ -174,11 +178,21 @@ def draw_6(NYCC_df,LA92_df):
     plt.axis([0, 500, -60, 40])
     plt.xticks(fontsize=15)
     plt.yticks(fontsize=15)
-    SC_SOC = plt.plot(t1, df['LA92  power[kw]'].values, 'g', t1, df['NYCC  power[kw]'].values, 'r--',linewidth=3)
+    SC_SOC = plt.plot(t1, df['LA92  power[kw]'].values, 'y', t1, df['NYCC  power[kw]'].values, 'r--',linewidth=3)
     plt.legend(handles = SC_SOC, labels = ['LA92', 'NYCC'], loc = 'best',fontsize=15)
     ax2.set_xlabel('Time[s]', fontsize=20)
     ax2.set_ylabel('Power[Kw]', fontsize=20)
     plt.show()
+    
+def add_guass(X_array,Y_array,sigma):
+    # 对输入数据加入gauss噪声
+    # 定义gauss噪声的均值和方差
+    mu = 0
+#    sigma = 0.1
+    for i in range(X_array.size):
+        X_array[i] += random.gauss(mu,sigma)
+        Y_array[i] += random.gauss(mu,sigma)
+    return X_array,Y_array
 
 if __name__ == '__main__' :
     #获取路况的数据。功率放缩过，缩小了13.2倍
@@ -187,35 +201,12 @@ if __name__ == '__main__' :
     # 产出图6
     draw_6(NYCC_df,LA92_df)
     # 产出图7
-    draw_7_8(NYCC_df)
+    draw_7_8(LA92_df)
     # 产出图8
-#    draw_7_8(LA92_df)
-
-    #验证I_Load*V_bus = EVPower
-#    draw_7_8(LA92_df)
-#    t1 = np.arange(0, 500, 0.1)
-#    plt.plot( t1,LA92_df['I_Load'].values * 24,t1,LA92_df['EVPower'].values)
-    
-#    print(LA92_df['I_Load'].values - LA92_df['I_Load.1'].values)
-#    
-#    v_bus_set = 24 * np.ones( (5000,1), dtype=np.int16 )
-#    print(LA92_df['bat_V'].values / 24)
-#    
-#    plt.figure(figsize = (15,5))
-#
-#    # 代码中的“...”代表省略的其他参数
-#    ax = plt.subplot(111)
-#    # 设置刻度字体大小
-#    plt.xticks(fontsize=20)
-#    plt.yticks(fontsize=20)
-#    # 设置坐标标签字体大小
-#    ax.set_xlabel('time', fontsize=20)
-#    ax.set_ylabel('speed', fontsize=20)
-#    # 设置图例字体大小
-#    ax.legend(..., fontsize=20)
-#
-#
-
+    draw_7_8(NYCC_df)
+    # todo
+    # 可以用台式机跑哇？
+    # 本次数据趋势不太 OK
     
     
     
